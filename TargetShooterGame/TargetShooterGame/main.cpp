acf46e06d555e8e8fa0fa4a79dd46662668c2f0f
Hunter <hunter909@ymail.com>
2025-04-14 12:34:28 -0700

    added comment to main

--- TargetShooterGame/TargetShooterGame/main.cpp
+++ TargetShooterGame/TargetShooterGame/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    //this is a test comment. delete me later
     sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
