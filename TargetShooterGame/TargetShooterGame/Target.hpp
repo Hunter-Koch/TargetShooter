fa4ddb88f38315c5df46177fc03209d41df0fd0a
Reed Williams <reedjwilliams6@gmail.com>
2025-04-15 16:39:39 -0700

    Confirming class changesi

--- TargetShooterGame/TargetShooterGame/Target.hpp
+++ TargetShooterGame/TargetShooterGame/Target.hpp
@@ -1,12 +1,54 @@
-#include <SFML/Graphics.hpp>
+#ifndef TARGET
+#define TARGET
 
-class Target : sf::CircleShape{
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+
+
+//all objects have to be transformable and drawable to be rendered
+class Target : public sf::CircleShape
+{
+public:
+	Target() : sound(destroyEffect), sf::CircleShape(100)
+	{
+		this->setPosition(sf::Vector2f(0,0));
+		this->mScoreAwarded = 5;
+		this->destroyEffect.loadFromFile("Assets\\sounds\\destroy.mp3");
+		this->appearEffect.loadFromFile("Assets\\sounds\\appear.mp3");
+		this->texture.loadFromFile("Assets\\targets\\Basic\\Target 64x64.png");
+	}
+
+	Target(sf::Vector2f pos, const sf::Color& color) :
+		sf::CircleShape(100), sound(destroyEffect)
+	{
+		//this->setFillColor(color); //changes color of texture
+		this->setPosition(pos);
+
+		this->texture.loadFromFile("Assets\\targets\\Basic\\Target 64x64.png");
+		this->setTexture(&texture, true);
+
+		this->destroyEffect.loadFromFile("Assets\\sounds\\destroy.mp3");
+		this->appearEffect.loadFromFile("Assets\\sounds\\appear.mp3");
+
+		this->sound.setVolume(50);//ranges from 0-100
+		mScoreAwarded = 100;
+	}
+
+	int getScoreAwarded(void);
+	void setScoreAwarded(int& newScore);
+	void playDestroyEffect(void);
+	void playAppearEffect(void);
+
+	sf::Texture& getTexture();
 
 private:
-
-protected:
-
-public:
-
+	int mScoreAwarded;
+	sf::Texture texture;
+	sf::SoundBuffer destroyEffect;
+	sf::SoundBuffer appearEffect;
+	sf::Sound sound;
 };
+
+
+#endif // !TARGET
 
--- TargetShooterGame/TargetShooterGame/main.cpp
+++ TargetShooterGame/TargetShooterGame/main.cpp
@@ -1,14 +1,18 @@
 #include <SFML/Graphics.hpp>
 #include "Target.hpp"
+#include "miscFunctions.hpp"
+#include "Timer.hpp"
 
 int main()
 {
     //this is a test comment. delete me later
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    Target t;
+    Target t(sf::Vector2f(randRange(0, 1000), randRange(0, 500)), sf::Color::Cyan);//constructing a vector, can construct variables just to be used as a parameter
+
+    Timer time;
 
     while (window.isOpen())
     {
@@ -16,10 +20,39 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+
+
+            if (event->is < sf::Event::MouseButtonPressed>())
+            {
+                // transform the mouse position from window coordinates to world coordinates
+                sf::Vector2f mouse = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+                // retrieve the bounding box of the sprite
+
+                sf::FloatRect bounds = t.getGlobalBounds();
+
+                // hit test
+                if (bounds.contains(mouse))
+                {
+                    t.setPosition(sf::Vector2f(randRange(0, 1000), randRange(0, 500)));
+                    if (t.getRadius() == 5)
+                    {
+                        t.setRadius(100);
+                        time.stop();
+                    }
+                    else                        
+                    {    
+                        t.setRadius(t.getRadius() - 5);
+                        t.playDestroyEffect();
+                    }
+                }
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(t);
+        time.setTextStringFromFloat(time.getElapsedTime().asSeconds());
+        window.draw(time.getText());
         window.display();
     }
 }
